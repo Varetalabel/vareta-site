1533f0e1d815e3e3e2c16e97112baca6f23356dd
Varetalabel <bradcro10@gmail.com>
2026-03-30 15:16:08 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-function addToCart(name, price) {
+function addToCart(name, price){
   cart.push({name, price});
   localStorage.setItem("cart", JSON.stringify(cart));
   alert("Added to cart");
 }
 
-function loadCart() {
+function loadCart(){
   const cartDiv = document.getElementById("cart");
-  if (!cartDiv) return;
+  if(!cartDiv) return;
 
   cartDiv.innerHTML = "";
-
-  cart.forEach(item => {
-    const div = document.createElement("div");
+  cart.forEach(item=>{
+    let div = document.createElement("div");
     div.innerText = item.name + " - $" + item.price;
     cartDiv.appendChild(div);
   });
+}
+
+function clearCart(){
+  localStorage.removeItem("cart");
+  location.reload();
 }
 
 loadCart();
